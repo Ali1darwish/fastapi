27ff2c4d7c6fa4c6cf832b641ad0b2336e501e1a
Ali1darwish <98328339+Ali1darwish@users.noreply.github.com>
2025-12-10 05:01:13 +0200

    Implement YouTube audio download endpoint
    Added a new endpoint to download audio from YouTube URLs.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
-app = FastAPI()
 
-@app.get("/{name}")
-async def get_things(name: str):
-    return {"hi there": name}
+app= FastAPI()
+@app.get("/")
+def get(url: str):
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.get_audio_only()
+        i = stream.download()
+        return StreamingResponse(r.iter_content(chunk_size=1024*512), media_type="video/mp4a")
+    except Exception as e:
+        print(f"An error occurred: {e}")
