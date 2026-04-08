c5f784ef1589bf30ab4665a9aa05e065612d1456
Ali1darwish <98328339+Ali1darwish@users.noreply.github.com>
2026-04-08 20:47:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from router import router as posts_router
+from questions import router as questions_router
 
 app = FastAPI()
 
 app.include_router(posts_router)
+app.include_router(questions_router)
